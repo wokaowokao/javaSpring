0f13c08b254ed79dc31d15416e90ebab8379009b
v_hongzheng <v_hongzheng@baidu.com>
2019-06-13 13:18:02 +0800

    d

---
+++ src/main/java/StaticTest.java
@@ -0,0 +1,46 @@
+public class StaticTest {
+    public static int k = 0;
+    public static StaticTest t1 = new StaticTest("t1");
+    public static StaticTest t2 = new StaticTest("t2");
+    public static int i = print("i");
+    public static int n = 99;
+
+    public int j = print("j");
+
+
+
+    {
+        print("构造快");
+    }
+
+    static{
+        print("静态块");
+    }
+
+    public StaticTest(String str) {
+        System.out.println((++k) + ":" + str + " i=" + i + " n=" + n);
+        ++n;
+        ++i;
+    }
+
+    public static int print(String str) {
+        System.out.println((++k) + ":" + str + " i=" + i + " n=" + n);
+        ++i;
+        return ++n;
+    }
+
+    public static void main(String[] args) {
+        //StaticTest t = new StaticTest("init");
+
+        //类初始化 static代码块 只执行一次
+        //当执行main方法 会执行初始化此类
+        //执行 static 赋值
+        //public static int k = 0;
+        //public static StaticTest t1 = new StaticTest("t1");
+        //public static StaticTest t2 = new StaticTest("t2");
+        //public static int i = print("i");
+        //public static int n = 99;
+
+    }
+
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,49 @@
+public class Test {
+
+    static Test test = new Test();
+    int a = print("aaa");
+    static int b = print("000");
+    {
+        System.out.println("bbb");
+    }
+    Test(){
+        System.out.println("ddd");
+    }
+    {
+        System.out.println("ccc");
+    }
+    static {
+        System.out.println("111");
+    }
+    public static int print(String aa){
+        System.out.println(aa);
+        return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("222");
+        Test ab = new Test();
+        Test bb = new Test();
+    }
+}
+//先执行类的初始化 执行类 static变量 static方法
+//执行 {}代码块 构造方法
+/*
+        aaa
+        bbb
+        ccc
+        ddd
+
+        000
+        111
+        222
+
+        aaa
+        bbb
+        ccc
+        ddd
+
+        aaa
+        bbb
+        ccc
+        ddd
+        */
