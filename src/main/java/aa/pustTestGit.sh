935713401290a4fc6869f05a4c7a455ae4fafbe4
yawei <d>
2020-07-21 12:11:20 +0800

    just test 2

--- src/main/java/aa/pustTestGit.sh
+++ src/main/java/aa/pustTestGit.sh
@@ -2,7 +2,9 @@ echo "开始执行切换分支。。执行完之后等待2s"
 git checkout -
 sleep 5
 echo "开始分支pull,之后push,执行完之后等待1s"
-git pull origin b1
+#git pull origin b1
+git pull --progress --no-stat -v --progress origin b1
+
 sleep 4
 git git push
 echo '开始切换分支。。'
