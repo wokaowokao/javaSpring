56b0ae3f1a61d349799f7169fe583c49b45bd17f
yawei <d>
2020-07-21 10:48:46 +0800

    test 提交b1 222

---
+++ src/main/java/aa/pustTestGit.sh
@@ -0,0 +1,13 @@
+
+
+echo "开始执行切换分支。。执行完之后等待2s"
+git checkout -
+sleep 5
+echo "开始分支pull,之后push,执行完之后等待1s"
+git pull origin b1
+sleep 4
+git git push
+echo '开始切换分支。。'
+git checkout -
+sleep 1
+echo '执行完毕'
